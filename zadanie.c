ae61ebaba3dd0d8d8c8b0d3f284a3b3373f4b731
Bartłomiej Kufel <87674236+BartlomiejKufel@users.noreply.github.com>
2024-11-30 15:08:30 +0100

    dodanie podzielnosci do zadanie.c

--- zadanie.c
+++ zadanie.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+void podzielnosc(int x, int y){
+    if(x % y == 0)
+        printf("x jest podzielne przez y");
+    else
+        printf("x nie jest podzielne przez y");
+}
+
+
+
 int signum(int x){
     
     if(x==0)
@@ -19,6 +28,8 @@ int main()
     printf("\n%d", signum(3));
     printf("\n%d", signum(0));
     printf("\n%d", signum(-12));
+
+    podzielnosc(4,2);
     
     return 0;
 }
